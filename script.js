57164c5b2d438930511ab2b4400674e4fe3e46d4
BartoszekA <agnieszka.bartoszek@gmail.com>
2020-02-08 16:21:45 +0100

    js fix3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://hidden-inlet-09407.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'ttps://hidden-inlet-09407.herokuapp.com/v1/trello/';
+  const trelloApiRoot = 'https://hidden-inlet-09407.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
